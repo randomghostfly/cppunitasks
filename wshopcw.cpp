9c2533a9f70e26fb105bd92d033889471aa7f912
randomghostfly <51033530+randomghostfly@users.noreply.github.com>
2019-12-02 09:54:57 +0300

    Added solutions for the lastest wshop.
    Added solutions for the lastest wshop.

---
+++ wshopcw.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+
+std::vector<int> delete_zeroes(std::vector<int> vec) {
+    int new_index = 0;
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] != 0) {
+            vec[new_index] = vec[i];
+            new_index++;
+        }
+    }
+    vec.resize(new_index);
+    return vec;
+}
+
+void delete_col(std::vector<std::vector<int>> matrix) {
+    std::unordered_set<int> col;
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            if (matrix[i][j] == 0) {
+                col.insert(j);
+            }
+        }
+    }
+
+    for (int elem : col) {
+        for (int i = 0; i < matrix.size(); i++) {
+            matrix[i].erase(matrix[i].begin()+elem);
+        }
+    }
+
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            std::cout << matrix[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
